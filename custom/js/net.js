d20ce61cdc74494bd993756716f786560869077f
xzylogic <1062186337@qq.com>
2019-03-06 14:10:08 +0800

    0306接口对接（1）

--- custom/js/net.js
+++ custom/js/net.js
@@ -1,4 +1,15 @@
-const netRoot = 'http://localhost:9999';
+// const netRoot = 'http://localhost:9999/app/mock/30/';
+const netRoot = '/app/mock/30/';
+var ajaxRoot = '/gw/';
+// var ajaxRoot = 'http://localhost:9999/gw/';
+
 const net = {
-  scheduleTime: netRoot + '/app/mock/30/api/diagnosis/reservation/scheduleTime',  //预约陪诊时间
+  getAdServiceGuidelines: netRoot + 'getAdServiceGuidelines',  //服务须知
+  userAccountInfo: netRoot + 'userAccountInfo',  //用户信息
+  queryAdHospital: netRoot + 'queryAdHospital',  //医院查询
+  saveAdReservation: netRoot + 'saveAdReservation',  //预约
+  adReservationEvaluate: netRoot + 'adReservationEvaluate',  //评价
+  scheduleTime: netRoot + 'api/diagnosis/reservation/scheduleTime',  //预约陪诊时间
+  scanConsume: netRoot + 'pay-agency-api/wdepay/scanConsume',  //支付
+  userGetTokenByAuthCode: ajaxRoot + 'userGetTokenByAuthCode',//根据code换token
 };

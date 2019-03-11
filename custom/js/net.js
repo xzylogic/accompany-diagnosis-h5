6f98189b718d33b67ed6e77847324b1bb0bc0b93
xzylogic <1062186337@qq.com>
2019-03-11 14:24:43 +0800

    0311接口对接（1）

--- custom/js/net.js
+++ custom/js/net.js
@@ -1,8 +1,6 @@
-// const netRoot = 'http://localhost:9999/app/mock/30/';
-// var ajaxRoot = 'http://localhost:9999/gw/';
-
-var netRoot = '/app/mock/30/';
-var net_Root = '/adsa/';
+// var net_Root = '/adsa/';
+// var netRoot = '/app/mock/30/';
+var netRoot = '/gw/';
 var ajaxRoot = '/gw/';
 
 // const net = {
@@ -26,20 +24,21 @@ var ajaxRoot = '/gw/';
 
 var net = {
   // getAdServiceGuidelines: 'http://10.1.64.194/accompany-diagnosis-service-api/api/reservation/getServiceGuidelines',  //服务须知
-  getAdServiceGuidelines: net_Root + '/reservation/getServiceGuidelines',  //服务须知
+  // getAdServiceGuidelines: net_Root + '/reservation/getServiceGuidelines',  //服务须知
+  getAdServiceGuidelines: ajaxRoot + 'getAdServiceGuidelines',  //服务须知
   makeOrder: netRoot + 'adMakeOrder',  //生成订单
   userAccountInfo: netRoot + 'userAccountInfo',  //用户信息
   queryAdHospital: netRoot + 'queryAdHospital',  //医院查询
-  saveAdReservation: net_Root + '/reservation/save',  //预约
-  cancelAdReservation: net_Root + '/reservation/cancel',  //取消预约
-  adReservationEvaluate: net_Root + '/reservation/evaluate',  //评价
-  adViewEvaluate: net_Root + '/reservation/viewEvaluate',  //查看评价
+  saveAdReservation: netRoot + 'saveAdReservation',  //预约
+  cancelAdReservation: netRoot + 'cancelAdReservation',  //取消预约
+  adReservationEvaluate: netRoot + 'adReservationEvaluate',  //评价
+  adViewEvaluate: netRoot + 'adViewEvaluate',  //查看评价
   orderList: netRoot + 'adOrderList',  //陪诊订单列表
   orderDetail: netRoot + 'adOrderDetail',  //陪诊订单详情
   scheduleTime: netRoot + 'adScheduleTime',  //预约陪诊时间
   adOrderRefundment: netRoot + 'adOrderRefundment',  //退款
   adRescindRefundment: netRoot + 'adRescindRefundment',  //撤销申请退款
 
-  scanConsume: netRoot + 'pay-agency-api/wdepay/scanConsume',  //支付
+  scanConsume: ajaxRoot + 'pay-agency-api/wdepay/scanConsume',  //支付
   userGetTokenByAuthCode: ajaxRoot + 'userGetTokenByAuthCode',//根据code换token
 };
